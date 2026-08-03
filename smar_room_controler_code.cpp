4eb2f087e6d0fbe03ddaf4b619df9e7e51c8ba0b
LukasJ <lukasz.jeskoo@gmail.com>
2026-08-04 00:54:11 +0200

    Add files via upload
    baza sprzętowa

---
+++ smar_room_controler_code.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_GFX.h> 
+#include <Adafruit_SSD1306.h>
+
+#define I2C_SDA 8
+#define I2C_SCL 9
+
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+Adafruit_BME280 bme;
+
+
+
+int myFunction(int, int);
+
+void setup() {
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+}
+
+int myFunction(int x, int y) {
+  return x + y;
+}
